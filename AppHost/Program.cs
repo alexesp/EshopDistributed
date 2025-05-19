13137b4a7a5c4ee6bcd8929b6d6258c0a4ac1eba
alexesp <alexizaes@hotmail.com>
2025-05-19 23:57:11 +0200

    Agregar archivos de proyecto.

---
+++ AppHost/Program.cs
@@ -0,0 +1,22 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+//Backing Services
+var postgres = builder
+    .AddPostgres("postgres")
+    .WithPgAdmin()
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var catalogDb = postgres.AddDatabase("catalogdb");
+
+
+//Projects
+
+var catalog = builder
+    .AddProject<Projects.Catalog>("catalog")
+    .WithReference(catalogDb)
+    .WaitFor(catalogDb);
+
+//builder.AddProject<Projects.Catalog>("catalog");
+
+builder.Build().Run();
